3820c8dcf4b5ff670ba36e5dd51b346568e4836d
Sanphet Somjit <xsplaystation5@gmail.com>
2025-05-25 19:55:31 +0700

    Method

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
-
-age = 30;
-
-const sumnmarizeUser = (userName, userAge, userHasHobbies) => {
-    return (
-      'Name is ' +
-      userName +
-      ', age is ' +
-      userAge +
-      ' and the user has hobbies: ' +
-      userHasHobbies
-    );
+const person = {
+  name: 'Max',
+  age: 29,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
 };
 
-//const add = (a,b) => a + b;
-//const addOne = a => a + 1;
-const addRandom = () => 1 + 2;
-
-//console.log(add(1,2));
-//console.log(addOne(1));
-console.log(addRandom());
-
-console.log(sumnmarizeUser(name, age, hasHobbies));
+person.greet(); // Hi, I am undefined
