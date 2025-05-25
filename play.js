9f87987d9052c5ba0833a16b4a36026208f2169c
Sanphet Somjit <xsplaystation5@gmail.com>
2025-05-25 23:35:49 +0700

    Understancding Spread & Rest Operators

--- play.js
+++ play.js
@@ -6,6 +6,9 @@ const person = {
   }
 };
 
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
 const hobbies = ['Sports', 'Cooking'];
 //for (let hobby of hobbies) {
 //  console.log(hobby);
@@ -13,5 +16,12 @@ const hobbies = ['Sports', 'Cooking'];
 //console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
 //console.log(hobbies);
 
-hobbies.push('Programming');
-console.log(hobbies);
+//hobbies.push('Programming');
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => {
+  return args;
+}
+
+console.log(toArray(1, 2, 3, 4));
