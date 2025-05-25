1ab55d6909664771b3c0195c5b52af5bb67db63d
Sanphet Somjit <xsplaystation5@gmail.com>
2025-05-25 19:38:00 +0700

    function with arrow

--- play.js
+++ play.js
@@ -2,10 +2,9 @@ const name = 'Max';
 let age = 29;
 const hasHobbies = true;
 
-name = 'Maximilian';
 age = 30;
 
-function sumnmarizeUser(userName, userAge, userHasHobbies) {
+const sumnmarizeUser = (userName, userAge, userHasHobbies) => {
     return (
       'Name is ' +
       userName +
@@ -14,6 +13,14 @@ function sumnmarizeUser(userName, userAge, userHasHobbies) {
       ' and the user has hobbies: ' +
       userHasHobbies
     );
-}
+};
+
+//const add = (a,b) => a + b;
+//const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
+//console.log(add(1,2));
+//console.log(addOne(1));
+console.log(addRandom());
 
 console.log(sumnmarizeUser(name, age, hasHobbies));
