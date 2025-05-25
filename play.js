c202127a37322784ace0c10be2c31a945c4765dd
Sanphet Somjit <xsplaystation5@gmail.com>
2025-05-25 23:52:46 +0700

    Destructuring

--- play.js
+++ play.js
@@ -6,10 +6,22 @@ const person = {
   }
 };
 
-const copiedPerson = {...person};
-console.log(copiedPerson);
+const printName = ({ name }) => {
+  console.log(name);
+}
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson);
 
 const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+
 //for (let hobby of hobbies) {
 //  console.log(hobby);
 //}
